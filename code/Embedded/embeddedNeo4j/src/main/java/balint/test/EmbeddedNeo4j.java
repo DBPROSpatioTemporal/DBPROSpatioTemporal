292bbb13a0fd81f57e02f9206245db89463bbf88
Balint Rueb <balintturueb@campus-tu-berlin.de>
2021-01-07 13:45:37 +0100

    Embedded Neo4j with data from Richard

--- code/Embedded/embeddedNeo4j/src/main/java/balint/test/EmbeddedNeo4j.java
+++ code/Embedded/embeddedNeo4j/src/main/java/balint/test/EmbeddedNeo4j.java
@@ -42,6 +42,7 @@ import static org.neo4j.configuration.GraphDatabaseSettings.DEFAULT_DATABASE_NAM
 public class EmbeddedNeo4j
 {
     private static final Path databaseDirectory = Path.of( "target/neo4j-hello-db" );
+    
 
     public String greeting;
 
@@ -63,7 +64,11 @@ public class EmbeddedNeo4j
     public static void main( final String[] args ) throws IOException
     {
         EmbeddedNeo4j hello = new EmbeddedNeo4j();
+        
+        System.out.println(databaseDirectory.toAbsolutePath().toString());
+        
         hello.createDb();
+       
         while(true) {
         	Scanner scanner = new Scanner(System.in);
         	System.out.println("Shut down database Y/n?");
@@ -98,28 +103,26 @@ public class EmbeddedNeo4j
             // Database operations go here
             // end::transaction[]
             // tag::addData[]
-            firstNode = tx.createNode();
-            firstNode.setProperty( "message", "Hello, " );
-            secondNode = tx.createNode();
-            secondNode.setProperty( "message", "World!" );
-
-            relationship = firstNode.createRelationshipTo( secondNode, RelTypes.KNOWS );
-            relationship.setProperty( "message", "brave Neo4j " );
-            // end::addData[]
-
-            // tag::readData[]
-//            System.out.print( firstNode.getProperty( "message" ) );
-//            System.out.print( relationship.getProperty( "message" ) );
-//            System.out.print( secondNode.getProperty( "message" ) );
-            // end::readData[]
-
-            greeting = ( (String) firstNode.getProperty( "message" ) )
-                       + ( (String) relationship.getProperty( "message" ) )
-                       + ( (String) secondNode.getProperty( "message" ) );
-
-            // tag::transaction[]
+            tx.execute("CREATE CONSTRAINT ON (node:`UNIQUE IMPORT LABEL`) ASSERT (node.`UNIQUE IMPORT ID`) IS UNIQUE;");
             tx.commit();
         }
+        graphDb.executeTransactionally("UNWIND [{_id:0, properties:{s_bahn_gleise:\"2\", fernbahn_gleise:\"9\", name:\"Köln Hauptbahnhof\", labels:\":Station\"}}, {_id:1, properties:{s_bahn_gleise:\"4\", fernbahn_gleise:\"8\", name:\"Hamburg Hauptbahnhof\", labels:\":Station\"}}, {_id:2, properties:{s_bahn_gleise:\"2\", fernbahn_gleise:\"10\", name:\"Hannover Hauptbahnhof\", labels:\":Station\"}}, {_id:3, properties:{s_bahn_gleise:\"4\", fernbahn_gleise:\"2\", name:\"Berlin-Westkreuz\", labels:\":Station\"}}, {_id:4, properties:{s_bahn_gleise:\"4\", fernbahn_gleise:\"8\", name:\"Hamburg Altona\", labels:\":Station\"}}, {_id:5, properties:{S_Bahn_Gleise:4, name:\"Gesundbrunnen\", Fernbahn_Gleise:6, labels:\":Station\"}}, {_id:6, properties:{s_bahn_gleise:\"2\", fernbahn_gleise:\"32\", name:\"München Hauptbahnhof\", labels:\":Station\"}}, {_id:7, properties:{s_bahn_gleise:\"4\", fernbahn_gleise:\"18\", name:\"Nürnberg Hauptbahnhof\", labels:\":Station\"}}, {_id:8, properties:{s_bahn_gleise:\"2\", fernbahn_gleise:\"4\", name:\"Berlin-Spandau\", labels:\":Station\"}}, {_id:9, properties:{s_bahn_gleise:\"4\", fernbahn_gleise:\"6\", name:\"Berlin-Südkreuz\", labels:\":Station\"}}, {_id:10, properties:{s_bahn_gleise:\"2\", fernbahn_gleise:\"4\", name:\"Potsdam Hauptbahnhof\", labels:\":Station\"}}, {_id:11, properties:{s_bahn_gleise:\"4\", fernbahn_gleise:\"12\", name:\"Berlin Hauptbahnhof\", u_bahn_gleise:\"2\", labels:\":Station\"}}, {_id:12, properties:{s_bahn_gleise:\"2\", name:\"Neukölln\", labels:\":Station\"}}, {_id:13, properties:{S_Bahn_Gleise:6, name:\"Ostkreuz\", Fernbahn_Gleise:6, labels:\":Station\"}}, {_id:14, properties:{S_Bahn_Gleise:4, U_Bahn_Gleise:2, name:\"Friedrichsstraße\", labels:\":Station\", Fernbahn_Gleise:4}}] AS row\r\n"
+        		+ "CREATE (n:`UNIQUE IMPORT LABEL`{`UNIQUE IMPORT ID`: row._id}) SET n += row.properties;");
+        graphDb.executeTransactionally("UNWIND [{start: {_id:8}, end: {_id:3}, properties:{distanz:10, label:\"SBAHN\"}}, {start: {_id:9}, end: {_id:11}, properties:{distanz:8, label:\"SBAHN\"}}, {start: {_id:3}, end: {_id:11}, properties:{distanz:4, label:\"SBAHN\"}}, {start: {_id:10}, end: {_id:3}, properties:{distanz:30, label:\"SBAHN\"}}, {start: {_id:4}, end: {_id:1}, properties:{distanz:6, label:\"SBAHN\"}}, {start: {_id:5}, end: {_id:14}, properties:{distanz:5, label:\"SBAHN\"}}, {start: {_id:12}, end: {_id:9}, properties:{distanz:4, label:\"SBAHN\"}}, {start: {_id:9}, end: {_id:3}, properties:{distanz:10, label:\"SBAHN\"}}, {start: {_id:14}, end: {_id:13}, properties:{distanz:5, label:\"SBAHN\"}}, {start: {_id:14}, end: {_id:11}, properties:{distanz:2, label:\"SBAHN\"}}, {start: {_id:9}, end: {_id:14}, properties:{distanz:7, label:\"SBAHN\"}}, {start: {_id:5}, end: {_id:13}, properties:{distanz:7, label:\"SBAHN\"}}, {start: {_id:12}, end: {_id:13}, properties:{distanz:4, label:\"SBAHN\"}}, {start: {_id:3}, end: {_id:5}, properties:{distanz:7, label:\"SBAHN\"}}] AS row\r\n"
+        		+ "MATCH (start:`UNIQUE IMPORT LABEL`{`UNIQUE IMPORT ID`: row.start._id})\r\n"
+        		+ "MATCH (end:`UNIQUE IMPORT LABEL`{`UNIQUE IMPORT ID`: row.end._id})\r\n"
+        		+ "CREATE (start)-[r:SBAHN]->(end) SET r += row.properties;\r\n");
+        	
+        graphDb.executeTransactionally("UNWIND [{start: {_id:8}, end: {_id:12}, properties:{distanz:20, label:\"UBAHN\"}}] AS row\r\n"
+        		+ "MATCH (start:`UNIQUE IMPORT LABEL`{`UNIQUE IMPORT ID`: row.start._id})\r\n"
+        		+ "MATCH (end:`UNIQUE IMPORT LABEL`{`UNIQUE IMPORT ID`: row.end._id})\r\n"
+        		+ "CREATE (start)-[r:UBAHN]->(end) SET r += row.properties;\r\n");
+        graphDb.executeTransactionally("UNWIND [{start: {_id:0}, end: {_id:1}, properties:{distanz:357, label:\"FERNBAHN\"}}, {start: {_id:0}, end: {_id:2}, properties:{distanz:249, label:\"FERNBAHN\"}}, {start: {_id:0}, end: {_id:7}, properties:{distanz:337, label:\"FERNBAHN\"}}, {start: {_id:7}, end: {_id:9}, properties:{distanz:438, label:\"FERNBAHN\"}}, {start: {_id:2}, end: {_id:8}, properties:{distanz:235, label:\"FERNBAHN\"}}, {start: {_id:8}, end: {_id:11}, properties:{distanz:13, label:\"FERNBAHN\"}}, {start: {_id:1}, end: {_id:2}, properties:{distanz:151, label:\"FERNBAHN\"}}, {start: {_id:9}, end: {_id:11}, properties:{distanz:8, label:\"FERNBAHN\"}}, {start: {_id:4}, end: {_id:1}, properties:{distanz:6, label:\"FERNBAHN\"}}, {start: {_id:1}, end: {_id:8}, properties:{distanz:278, label:\"FERNBAHN\"}}, {start: {_id:6}, end: {_id:7}, properties:{distanz:173, label:\"FERNBAHN\"}}, {start: {_id:7}, end: {_id:2}, properties:{distanz:472, label:\"FERNBAHN\"}}, {start: {_id:10}, end: {_id:11}, properties:{distanz:34, label:\"FERNBAHN\"}}, {start: {_id:14}, end: {_id:13}, properties:{distanz:5, label:\"FERNBAHN\"}}, {start: {_id:14}, end: {_id:11}, properties:{distanz:2, label:\"FERNBAHN\"}}, {start: {_id:9}, end: {_id:14}, properties:{distanz:7, label:\"FERNBAHN\"}}] AS row\r\n"
+        		+ "MATCH (start:`UNIQUE IMPORT LABEL`{`UNIQUE IMPORT ID`: row.start._id})\r\n"
+        		+ "MATCH (end:`UNIQUE IMPORT LABEL`{`UNIQUE IMPORT ID`: row.end._id})\r\n"
+        		+ "CREATE (start)-[r:FERNBAHN]->(end) SET r += row.properties;");
+        graphDb.executeTransactionally("MATCH (n:`UNIQUE IMPORT LABEL`)  WITH n LIMIT 20000 REMOVE n:`UNIQUE IMPORT LABEL` REMOVE n.`UNIQUE IMPORT ID`;");
+        graphDb.executeTransactionally("DROP CONSTRAINT ON (node:`UNIQUE IMPORT LABEL`) ASSERT (node.`UNIQUE IMPORT ID`) IS UNIQUE;");
         // end::transaction[]
     }
 
@@ -129,13 +132,7 @@ public class EmbeddedNeo4j
         {
             // tag::removingData[]
             // let's remove the data
-            firstNode = tx.getNodeById( firstNode.getId() );
-            secondNode = tx.getNodeById( secondNode.getId() );
-            firstNode.getSingleRelationship( RelTypes.KNOWS, Direction.OUTGOING ).delete();
-            firstNode.delete();
-            secondNode.delete();
-            // end::removingData[]
-
+            tx.execute("MATCH (n)-[r]->(m) delete n,r,m;");
             tx.commit();
         }
     }
